9516bd6f063813a8091e775d5a6c11687cf8921f
msamgan <mohdsamgankhan@gmail.com>
2020-10-27 22:38:29 +0400

    post updated with tags and date

---
+++ helpers/methods.php
@@ -0,0 +1,28 @@
+<?php
+
+if (!function_exists('comaSeparateTags')) {
+    /**
+     * @param $tags
+     * @return string
+     */
+    function comaSeparateTags($tags)
+    {
+        $tagString = [];
+        foreach ($tags as $tag) {
+            $tagString[] = '<a href="'. url('tag/' . $tag->slug) .'" >'. $tag->name .'</a>';
+        }
+
+        return implode(', ', $tagString);
+    }
+}
+
+if (!function_exists('formatDate')) {
+    /**
+     * @param $date
+     * @return false|string
+     */
+    function formatDate($date)
+    {
+        return date('F d, Y', strtotime($date));
+    }
+}
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,9 +5,12 @@
 @section('content')
     <div class="row mb-2 mt-5">
         <div class="col-md-12 blog-main">
+            <h6 class="pb-4 mb-4 border-bottom">
+                {!! comaSeparateTags($post->tags)  !!}
+            </h6>
             <div class="blog-post">
                 <h2 class="blog-post-title">{{ $post->title }}</h2>
-                <p class="blog-post-meta mt-2">January 1, 2014 by <a href="#">{{ $post->author->name }}</a></p>
+                <p class="blog-post-meta mt-2">{{ formatDate($post->created_at) }} by <a href="#">{{ $post->author->name }}</a></p>
 
                 {!! $post->body  !!}
             </div>

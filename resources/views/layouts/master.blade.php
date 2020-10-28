92d1ac5b9863c0b59b7279111f7c0535608fdb96
msamgan <mohdsamgankhan@gmail.com>
2020-10-28 13:02:58 +0400

    updated the link color

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,15 +4,32 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v4.1.1">
+    <meta name="author" content="msamgan">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicons/apple-icon-57x57.png') }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ asset('favicons/apple-icon-60x60.png') }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ asset('favicons/apple-icon-72x72.png') }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('favicons/apple-icon-76x76.png') }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ asset('favicons/apple-icon-114x114.png') }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('favicons/apple-icon-120x120.png') }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('favicons/apple-icon-144x144.png') }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('favicons/apple-icon-152x152.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicons/apple-icon-180x180.png') }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ asset('favicons/android-icon-192x192.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('favicons/favicon-96x96.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('favicons/manifest.json') }}">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="{{ asset('favicons/ms-icon-144x144.png') }}">
+    <meta name="theme-color" content="#ffffff">
+
     <title>Samgan's Journal - @yield('title')</title>
-    <!-- Custom styles for this template -->
-    {{--<link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">--}}
-    <!-- Custom styles for this template -->
+
     <link href="{{ asset('css/all.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Special+Elite&display=swap" rel="stylesheet">
-    <script async src="https://cse.google.com/cse.js?cx=0b2263754f91e5d8f"></script>
+    <link rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.1/styles/default.min.css">
 </head>
 <body>
 <div class="container-fluid container-width mt-4">
@@ -24,6 +41,9 @@
 </div>
 
 @include('partials.front.footer')
+
+<script async src="https://cse.google.com/cse.js?cx=0b2263754f91e5d8f"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.3.1/highlight.min.js"></script>
 @stack('script')
 </body>
 </html>
--- resources/views/partials/front/footer.blade.php
+++ resources/views/partials/front/footer.blade.php
@@ -7,8 +7,8 @@
         </nav>
     </div>
     <p>
-        Created with Love by <a href="https://github.com/msamgan" target="_blank">msamgan</a>
+        Created with Love by <a  href="https://github.com/msamgan" rel="noopener" target="_blank">msamgan</a>
         <br>
-        This site was built using <a href="https://github.com/themsaid/wink" target="_blank">Wink</a>
+        This site was built using <a href="https://github.com/themsaid/wink" rel="noopener" target="_blank">Wink</a>
     </p>
 </footer>

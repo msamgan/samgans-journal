408f31f5b9d96acd9f586113bc4db8001c6576bc
msamgan <mohdsamgankhan@gmail.com>
2020-10-30 18:55:06 +0400

    removed slash

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\TagController;
+use App\Models\Tag;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,18 @@ Route::get('/{post}', [PostController::class, 'post'])->name('post');
 Route::get('/tag/{tag}', [TagController::class, 'posts'])->name('tag.posts');
 Route::get('/page/{page}', [PageController::class, 'page'])->name('page');
 Route::get('/author/{author}', [AuthorController::class, 'authorPage'])->name('author.page');
+
+/**
+ * redirecting category links to tag.
+ */
+Route::get('category/{tag}', function (Tag $tag) {
+    return redirect('/tag/' . $tag->slug);
+});
+
+/**
+ * redirecting sub category links to tag.
+ */
+Route::get('category/{tagPrimary}/{tag}', function (Tag $tagPrimary, Tag $tag) {
+    return redirect('/tag/' . $tag->slug);
+});
+

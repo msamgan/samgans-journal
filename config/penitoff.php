0b673d57491420a672efae8da38a4b080633e6e9
msamgan <mohdsamgankhan@gmail.com>
2021-03-02 22:11:57 +0400

    added project list

---
+++ config/penitoff.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'key' => env('PEN_IT_OFF_KEY', null),
+];
---
+++ resources/views/projects.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.master')
+
+@section('title', 'Projects')
+
+@section('metaDescription', 'Project list')
+
+@section('content')
+    <div class="row mb-2 mt-2">
+        @foreach($projects as $project)
+            <div class="col-md-12 zoom">
+                <div class="row no-gutters border rounded  mb-4 shadow-sm">
+                    <div class="col p-3 d-flex position-static">
+                        @if ($project->logo_image)
+                            <img alt="{{ $project->name }}" class="p-4 w-25 blog-img-height"
+                                 src="{{ $project->logo_image }}">
+                        @endif
+                        {{--<div class="mb-1 text-muted">{{ formatDate($project->publish_date) }}</div>
+                        <p class="d-inline-block mb-2 text-primary">
+                            {!! comaSeparateTags($project->tags) !!}
+                        </p>--}}
+                        <div class="flex-column pl-5">
+                            <a href="#{{--{{ url('project/' . $project->slug . '/details') }}--}}">
+                                <h1 class="mb-2 listing-blog-title">{{ ucwords($project->name) }}</h1>
+                            </a>
+                            @if($project->excerpt)
+                                <p class="card-text mb-auto pb-1">
+                                    {{ $project->excerpt }}
+                                </p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+        <div class="m-auto">
+            @if($projects instanceof \Illuminate\Pagination\LengthAwarePaginator)
+                {{ $projects->links() }}
+            @endif
+        </div>
+    </div>
+@stop
